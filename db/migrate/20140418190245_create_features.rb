5700104c6370d76eb9d90b4b6f897f26646aa5d9
ubergesundheit <ubergesundheit@googlemail.com>
2014-04-25 18:58:17 +0200

    remove db gitignore and check in db files.

---
+++ db/migrate/20140418190245_create_features.rb
@@ -0,0 +1,14 @@
+class CreateFeatures < ActiveRecord::Migration
+  def change
+    #enable_extension "hstore"
+    create_table :features do |t|
+      t.geometry :geom, :geographic => true, :srid=> 4326, :null => false
+
+      t.hstore :properties
+
+      t.timestamps
+
+      t.index :geom, :spatial => true
+    end
+  end
+end
