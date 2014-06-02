aac900daea8f75d1a618313a0482a746dcf87642
ubergesundheit <ubergesundheit@googlemail.com>
2014-06-02 20:55:29 +0200

    force_ssl and x-sendfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,10 +37,10 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
