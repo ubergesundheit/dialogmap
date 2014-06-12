c13c2aa50b02380f247ec4070b4993e133fd8771
ubergesundheit <ubergesundheit@googlemail.com>
2014-06-12 18:28:17 +0200

    migration - add deleted and deleted_reason to contributions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508102330) do
+ActiveRecord::Schema.define(version: 20140612162359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20140508102330) do
     t.integer  "parent_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "deleted"
+    t.string   "delete_reason"
   end
 
   create_table "features", force: true do |t|
