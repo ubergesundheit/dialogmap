a24e92ed0ca8822edc3633937a0d9996e316aee6
ubergesundheit <ubergesundheit@googlemail.com>
2014-06-22 15:00:42 +0200

    show category color in contributions index

--- app/admin/contribution.rb
+++ app/admin/contribution.rb
@@ -2,6 +2,21 @@ ActiveAdmin.register Contribution do
 
   permit_params :title, :description, :deleted, :delete_reason, :properties, :category_color
 
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :description
+    column :created_at
+    column :updated_at
+    column :deleted
+    column :delete_reason
+    column :category do |color|
+      raw("<span class=\"status_tag\" style=\"color: black;border:1px solid black;background:#{color.category_color};\">#{color.category}</span>")
+    end
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :title
