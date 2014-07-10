916e604d9be86ce0536f487f7271b69a9c08ea47
ubergesundheit <ubergesundheit@googlemail.com>
2014-07-10 16:37:42 +0200

    update chef recipe for new environment vars

--- INSTALL.md
+++ INSTALL.md
@@ -6,14 +6,16 @@
     - or
     - ssh into your host
     - append your ssh-key to the `authorized_keys` file
+  * `cd` into the `infrastructure` folder
   * `knife solo prepare user@host`
   * modify `nodes/host.json`. Use the `node.json.example` as base.
   * look at the vhost template if you want to use ssl, or ask someone who knows nginx and chef..
-  * `knife solo cook user@host` - this will take long and show some errors but will run through
+  * `knife solo cook user@host` - this will take long and show some warnings but will run through
 
 ## Deployment (installing the app to the server)
 
 #### In project root
+  * `cd` to the project root
   * modify `config/deploy/production.rb` - change the ip/hostname of your host
   * modify `config/deploy.rb` - normally you don't have to change things in here
   * `bundle exec cap production deploy`
--- infrastructure/site-cookbooks/dialogmap-cookbook/recipes/shared_files.rb
+++ infrastructure/site-cookbooks/dialogmap-cookbook/recipes/shared_files.rb
@@ -15,7 +15,19 @@ template(File.join(node["users"]["apps_dir"], node["app"]["appname"], "shared", 
   variables(
     devise_secret_key: node["app"]["devise_secret_key"],
     secret_key_base: node["app"]["secret_key_base"],
-    database_password: node["postgresql"]["database"]["password"]
+    database_password: node["postgresql"]["database"]["password"],
+    oauth_facebook_key: node["app"]["oauth"]["facebook"]["key"],
+    oauth_facebook_secret: node["app"]["oauth"]["facebook"]["secret"],
+    oauth_twitter_key: node["app"]["oauth"]["twitter"]["key"],
+    oauth_twitter_secret: node["app"]["oauth"]["twitter"]["secret"],
+    oauth_google_key: node["app"]["oauth"]["google"]["key"],
+    oauth_google_secret: node["app"]["oauth"]["google"]["secret"],
+    hostname: node["app"]["hostname"],
+    smtp_host: node["app"]["email"]["smtp"]["host"],
+    smtp_port: node["app"]["email"]["smtp"]["port"],
+    smtp_user: node["app"]["email"]["smtp"]["user"],
+    smtp_password: node["app"]["email"]["smtp"]["password"],
+    email_from: node["app"]["email"]["email_from"]
   )
   action :create_if_missing
 end
