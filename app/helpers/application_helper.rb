e7ce52b066ef8aaeeedd8cc07d48e90d02bdbe54
ubergesundheit <ubergesundheit@googlemail.com>
2014-10-05 12:16:24 +0200

    include layout and some helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def html_title
+    "#{@cms_page.try(:label)} | Wir machen mit" || 'Wir machen mit'
+  end
+
+  def active_menu_item_class(label)
+    "active" if label == @cms_page.label
+  end
+
+  def menu_items
+    @cms_site.pages.map do |page|
+      content_tag('li', class: active_menu_item_class(page.label)) do
+        link_to page.label, page.full_path
+      end
+    end.join.html_safe
+  end
 end
